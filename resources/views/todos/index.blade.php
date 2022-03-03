4f0cca3f69592007bbb552bfd83c9d7f71560767
Serfin01 <sergilozano96@gmail.com>
2022-03-03 22:29:30 +0100

    formulario feo

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,16 @@
+@extends('mainPc')
+
+@section('content')
+
+    <div class="container w-25 border p-4 mt-5">
+        <form>
+            <div class="mb-3">
+                <label for="tittle" class="form-label">Entérate del tiempo en la zona exacta que te interesa buscando por código postal.</label>
+                <input type="text" name="tittle" placeholder="Introduce el código postal" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Buscar</button>
+                <div class="form-text">¡Que la lluvia no te pare!</div>
+        </form>
+    </div>
+
+@endsection
